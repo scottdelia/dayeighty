82d5f0753ca82d712dea848665e48477d5501242
Scott Delia <scott.delia@gmail.com>
2026-09-15 15:34:26 -0500

    Stripboard: time as a tag, set name alone

--- assets/node.browser-BR0g4Nj-.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./index-Beo7Cohx.js";function t(n){return new Proxy({},{get(r,e){if(typeof e!="symbol")throw new o(`\`${n}.${e}\` is not available in this environment; it needs a Node.js-compatible runtime`)}})}t("child_process");t("crypto");const s=t("fs");t("os");const a=t("path");t("stream");t("util");export{s as fs,a as path};
